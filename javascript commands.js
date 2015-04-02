1eab92d3f7196ad60ff25fbd682a24052bade83d
Manni-T-xX2006Xx <Manni-T-xX2006Xx@users.noreply.github.com>
2015-04-02 18:55:24 +0200

    Update javascript commands.js

--- javascript commands.js
+++ javascript commands.js
@@ -1,5 +1,5 @@
 /* */
 javascript:var inputs = document.getElementsByClassName('_3hqx'); for(var i=0; i<inputs.length;i++) { inputs[i].click() };
 
-/* */
+/* Für Veranstaltungen folgenden Code */
 javascript:elms=document.getElementsByName("checkableitems[]");for (i=0;i<elms.length;i++){if (elms[i].type="checkbox" )elms[i].click()}
