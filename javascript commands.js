6a122109b4ffe2e84b947f3063b1ae439114dd6f
Manni-T-xX2006Xx <Manni-T-xX2006Xx@users.noreply.github.com>
2015-04-02 18:54:54 +0200

    javascript commands

---
+++ javascript commands.js
@@ -0,0 +1,5 @@
+/* */
+javascript:var inputs = document.getElementsByClassName('_3hqx'); for(var i=0; i<inputs.length;i++) { inputs[i].click() };
+
+/* */
+javascript:elms=document.getElementsByName("checkableitems[]");for (i=0;i<elms.length;i++){if (elms[i].type="checkbox" )elms[i].click()}
