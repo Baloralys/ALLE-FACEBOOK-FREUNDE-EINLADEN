8b50f18c92b97289b0ac832fe3064d86fa03255c
Manni-T-xX2006Xx <Manni-T-xX2006Xx@users.noreply.github.com>
2015-04-02 18:56:36 +0200

    Update javascript commands.js

--- javascript commands.js
+++ javascript commands.js
@@ -1,5 +1,2 @@
-/* Für Facebook-Seiten folgenden Code */
-javascript:var inputs = document.getElementsByClassName('_3hqx'); for(var i=0; i<inputs.length;i++) { inputs[i].click() };
-
 /* Für Veranstaltungen folgenden Code */
 javascript:elms=document.getElementsByName("checkableitems[]");for (i=0;i<elms.length;i++){if (elms[i].type="checkbox" )elms[i].click()}
