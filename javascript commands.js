6a5f5bbc519a83ffb111a3c311eb1a3a5a8ef2df
Manni-T-xX2006Xx <Manni-T-xX2006Xx@users.noreply.github.com>
2015-04-02 18:55:44 +0200

    Update javascript commands.js

--- javascript commands.js
+++ javascript commands.js
@@ -1,4 +1,4 @@
-/* */
+/* Für Facebook-Seiten folgenden Code */
 javascript:var inputs = document.getElementsByClassName('_3hqx'); for(var i=0; i<inputs.length;i++) { inputs[i].click() };
 
 /* Für Veranstaltungen folgenden Code */
