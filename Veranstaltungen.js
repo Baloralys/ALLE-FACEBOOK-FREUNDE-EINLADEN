308331e4af34e79530c0178fe2aef4e2cf649238
Baloralys <Manni-T-xX2006Xx@users.noreply.github.com>
2015-04-03 04:38:07 +0200

    Update Veranstaltungen.js

--- Veranstaltungen.js
+++ Veranstaltungen.js
@@ -1,3 +1,2 @@
 /* Für Veranstaltungen folgenden Code */
-javascript:elms=document.getElementsByName("checkableitems[]");for (i=0;i<elms.length;i++){if (elms[i].type="checkbox" )elms[i].click()}
-; alert('Fertig, all deine Freunde wurden selektiert!');
+javascript:elms=document.getElementsByName("checkableitems[]");for (i=0;i<elms.length;i++){if (elms[i].type="checkbox" )elms[i].click()}; alert('Fertig, all deine Freunde wurden selektiert!');
