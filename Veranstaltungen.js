a14c132c48415dcc16622aaea600e140559aef6d
Baloralys <Manni-T-xX2006Xx@users.noreply.github.com>
2015-04-03 04:37:33 +0200

    Update Veranstaltungen.js

--- Veranstaltungen.js
+++ Veranstaltungen.js
@@ -1,3 +1,3 @@
 /* Für Veranstaltungen folgenden Code */
 javascript:elms=document.getElementsByName("checkableitems[]");for (i=0;i<elms.length;i++){if (elms[i].type="checkbox" )elms[i].click()}
-;; alert('Fertig, all deine Freunde wurden selektiert!');
+; alert('Fertig, all deine Freunde wurden selektiert!');
